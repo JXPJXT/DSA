2031c3f5c238edc54801bfdd0a9a394816bb7307
JXPJXT <samjj317@gmail.com>
2025-08-30 14:47:06 +0530

    v33

---
+++ 3.Day-2(Linked List Problems)/6.MiddleOfLL(LeetCode-876)Easy/MiddleOfLL.java
@@ -0,0 +1,60 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
+class MiddleOfLL {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        // Test case 1: Odd number of nodes
+        ListNode head1 = new ListNode(1);
+        head1.next = new ListNode(2);
+        head1.next.next = new ListNode(3);
+        head1.next.next.next = new ListNode(4);
+        head1.next.next.next.next = new ListNode(5);
+        System.out.println("Original list:");
+        printList(head1);
+        ListNode middle1 = solution.middleNode(head1);
+        System.out.println("Middle node value: " + middle1.val);
+        System.out.println();
+
+        // Test case 2: Even number of nodes
+        ListNode head2 = new ListNode(1);
+        head2.next = new ListNode(2);
+        head2.next.next = new ListNode(3);
+        head2.next.next.next = new ListNode(4);
+        head2.next.next.next.next = new ListNode(5);
+        head2.next.next.next.next.next = new ListNode(6);
+        System.out.println("Original list:");
+        printList(head2);
+        ListNode middle2 = solution.middleNode(head2);
+        System.out.println("Middle node value: " + middle2.val);
+        System.out.println();
+    }
+
+    public static void printList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val);
+            if (current.next != null) {
+                System.out.print(" -> ");
+            }
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
