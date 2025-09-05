3173d1a7df9aae489574b9c533b3004869ef572b
JXPJXT <samjj317@gmail.com>
2025-09-05 12:31:25 +0530

    v77

---
+++ 6.Day-6(Hashing & SetsMaps Problems)/1.TwoSum(LeetCode-1)Easy/TwoSum.java
@@ -0,0 +1,41 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Arrays;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[]{numMap.get(complement), i};
+            }
+            numMap.put(nums[i], i);
+        }
+
+        return new int[]{};
+    }
+}
+
+public class TwoSum {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        // Example 1
+        int[] nums1 = {2, 7, 11, 15};
+        int target1 = 9;
+        int[] result1 = solution.twoSum(nums1, target1);
+        System.out.println("Input: nums = " + Arrays.toString(nums1) + ", target = " + target1);
+        System.out.println("Output: " + Arrays.toString(result1));
+
+        // Example 2
+        int[] nums2 = {3, 2, 4};
+        int target2 = 6;
+        int[] result2 = solution.twoSum(nums2, target2);
+        System.out.println("\nInput: nums = " + Arrays.toString(nums2) + ", target = " + target2);
+        System.out.println("Output: " + Arrays.toString(result2));
+    }
+}
+v
