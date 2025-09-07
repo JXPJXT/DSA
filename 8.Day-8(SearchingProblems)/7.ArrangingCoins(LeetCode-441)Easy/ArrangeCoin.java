88b27876f8936f3096a98ff405b1da6e3cbea3c9
JXPJXT <samjj317@gmail.com>
2025-09-07 11:21:59 +0530

    v101

---
+++ 8.Day-8(SearchingProblems)/7.ArrangingCoins(LeetCode-441)Easy/ArrangeCoin.java
@@ -0,0 +1,23 @@
+class ArrangeCoin {
+    public int arrangeCoins(int n) {
+        int s = 1;
+        int e = n;
+        while(s<=e){
+            int mid = s + (e-s)/2;
+            long sum = (long)mid*(mid+1)/2;
+            if(sum == n) return mid;
+            else if(sum > n) e = mid-1;
+            else s = mid+1;
+        }
+        return s-1;
+    }
+
+    public static void main(String[] args) {
+        Main solution = new Main();
+        int n1 = 5;
+        System.out.println("For n = " + n1 + ", the number of full rows is: " + solution.arrangeCoins(n1));
+        
+        int n2 = 8;
+        System.out.println("For n = " + n2 + ", the number of full rows is: " + solution.arrangeCoins(n2));
+    }
+}
