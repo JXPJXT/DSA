7c4e55d2b88456d1a53fb223982a0f4ef38bc3d2
JXPJXT <samjj317@gmail.com>
2025-09-07 12:26:35 +0530

    v110

---
+++ 10.Day-9(Mixed Fundamentals Practice)/1.TwoSum(LeetCode-1)Easy/TwoSum.java
@@ -0,0 +1,44 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (numMap.containsKey(diff)) {
+                return new int[]{numMap.get(diff), i};
+            }
+            numMap.put(nums[i], i);
+        }
+        return new int[0];
+    }
+}
+
+public class TwoSumOptimized {
+    public static void main(String[] args) {
+        TwoSum solution = new TwoSum();
+
+        int[] nums1 = {2, 7, 11, 15};
+        int target1 = 9;
+        int[] result1 = solution.twoSum(nums1, target1);
+        System.out.println("Input: nums = [2, 7, 11, 15], target = 9");
+        System.out.println("Output: [" + result1[0] + ", " + result1[1] + "]");
+
+        System.out.println("--------------------");
+
+        int[] nums2 = {3, 2, 4};
+        int target2 = 6;
+        int[] result2 = solution.twoSum(nums2, target2);
+        System.out.println("Input: nums = [3, 2, 4], target = 6");
+        System.out.println("Output: [" + result2[0] + ", " + result2[1] + "]");
+
+        System.out.println("--------------------");
+
+        int[] nums3 = {3, 3};
+        int target3 = 6;
+        int[] result3 = solution.twoSum(nums3, target3);
+        System.out.println("Input: nums = [3, 3], target = 6");
+        System.out.println("Output: [" + result3[0] + ", " + result3[1] + "]");
+    }
+}
