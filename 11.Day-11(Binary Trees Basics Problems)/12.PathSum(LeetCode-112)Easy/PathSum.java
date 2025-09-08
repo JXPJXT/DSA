7eb300faa180e90b7afc6c9f7cc81aff4e538a1f
JXPJXT <samjj317@gmail.com>
2025-09-08 10:51:50 +0530

    v124

---
+++ 11.Day-11(Binary Trees Basics Problems)/12.PathSum(LeetCode-112)Easy/PathSum.java
@@ -0,0 +1,43 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return targetSum - root.val == 0;
+        }
+        targetSum -= root.val;
+        return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);
+    }
+}
+public class PathSum {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        TreeNode root1 = new TreeNode(5);
+        root1.left = new TreeNode(4);
+        root1.right = new TreeNode(8);
+        root1.left.left = new TreeNode(11);
+        root1.left.left.left = new TreeNode(7);
+        root1.left.left.right = new TreeNode(2);
+        root1.right.left = new TreeNode(13);
+        root1.right.right = new TreeNode(4);
+        root1.right.right.right = new TreeNode(1);
+        int targetSum1 = 22;
+        System.out.println("Does the tree have a path sum of " + targetSum1 + "? " + solution.hasPathSum(root1, targetSum1));
+        TreeNode root2 = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+        int targetSum2 = 5;
+        System.out.println("Does the tree have a path sum of " + targetSum2 + "? " + solution.hasPathSum(root2, targetSum2));
+    }
+}
