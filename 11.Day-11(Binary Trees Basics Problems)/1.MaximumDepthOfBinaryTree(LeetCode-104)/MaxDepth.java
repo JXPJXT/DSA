970e9b00aaaf0bfb22b0e074fb1946351febd9eb
JXPJXT <samjj317@gmail.com>
2025-09-08 10:01:06 +0530

    v112

---
+++ 11.Day-11(Binary Trees Basics Problems)/1.MaximumDepthOfBinaryTree(LeetCode-104)/MaxDepth.java
@@ -0,0 +1,46 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {}
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        return Math.max(left, right) + 1;
+    }
+}
+
+public class MaxDepth {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        Solution solution = new Solution();
+
+        int depth = solution.maxDepth(root);
+
+        System.out.println("The maximum depth of the binary tree is: " + depth);
+    }
+}
