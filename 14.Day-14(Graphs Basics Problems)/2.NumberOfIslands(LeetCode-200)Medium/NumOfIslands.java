4656868663b53678f73261ac43d3d2eaafefbe28
JXPJXT <samjj317@gmail.com>
2025-09-11 14:13:42 +0530

    v156

---
+++ 14.Day-14(Graphs Basics Problems)/2.NumberOfIslands(LeetCode-200)Medium/NumOfIslands.java
@@ -0,0 +1,47 @@
+class NumOfIslands {
+    public int numIslands(char[][] grid) {
+        if (grid == null) {
+            return 0;
+        }
+        int numIslands = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    numIslands++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return numIslands;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != '1') {
+            return;
+        }
+        grid[i][j] = '0';
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    }
+
+    public static void main(String[] args) {
+        NumOfIsland solution = new NumOfIsland();
+        char[][] grid1 = {
+            {'1', '1', '1', '1', '0'},
+            {'1', '1', '0', '1', '0'},
+            {'1', '1', '0', '0', '0'},
+            {'0', '0', '0', '0', '0'}
+        };
+        System.out.println("Number of islands: " + solution.numIslands(grid1)); // Expected output: 1
+
+        char[][] grid2 = {
+            {'1', '1', '0', '0', '0'},
+            {'1', '1', '0', '0', '0'},
+            {'0', '0', '1', '0', '0'},
+            {'0', '0', '0', '1', '1'}
+        };
+        System.out.println("Number of islands: " + solution.numIslands(grid2)); // Expected output: 3
+    }
+}
